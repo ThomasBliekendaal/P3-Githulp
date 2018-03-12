bf0797c39802031f7809c5a54de2c36bc63064ff
Dilar <dilar1_4@hotmail.com>
2018-03-12 14:13:12 +0100

    Scene gemaakt en een paar scripts
    Yup

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Dilar/DrScripts/DrEnemyMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrEnemyMaster : MonoBehaviour
+{
+    public int health;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    
+    public void Death()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Dilar/DrScripts/DrMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrMovement : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Vector3 move;
+    public Vector3 rotateValue;
+    public Vector3 rotateValueCamera;
+    public int sensitivity;
+    public int walkSpeed;
+    public int sprintSpeed;
+    public int walkReset;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotator();
+        Movement();
+    }
+
+    private void Movement()
+    {
+        move.z = Input.GetAxis("Vertical") * walkSpeed;
+        move.x = Input.GetAxis("Horizontal") * walkSpeed;
+        move *= Time.deltaTime;
+        transform.Translate(move);
+
+        if (Input.GetButton("Sprint"))
+        {
+            walkSpeed = (walkSpeed = sprintSpeed);
+        }
+        else
+        {
+            walkSpeed = walkReset;
+        }
+    }
+
+    private void CameraRotator()
+    {
+        rotateValue.x = -Input.GetAxis("Mouse Y");
+        rotateValueCamera.y = Input.GetAxis("Mouse X");
+        transform.Rotate(rotateValueCamera);
+        playerCamera.transform.Rotate(rotateValue);
+    }
+}
