95a8f0ba9b66f521236a786b775d9895753849ce
Dilar <dilar1_4@hotmail.com>
2018-03-15 11:17:10 +0100

    Kleine update

---
+++ P3 Videospelletje/P3 Schpiel/Assets/OneHandedMelee.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneHandedMelee : MonoBehaviour
+{
+    public Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("ItWorks");
+            animator.SetBool("MBAttack", true);
+            animator.SetBool("MBAttack", false);
+        }
+    }
+}
