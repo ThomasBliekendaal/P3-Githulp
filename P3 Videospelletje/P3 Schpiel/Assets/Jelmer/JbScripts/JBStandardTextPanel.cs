52645add56955eedaff1408f335a85667b8eef89
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-03-15 14:11:50 +0100

    Added basic text
    the name

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Jelmer/JbScripts/JBStandardTextPanel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JBStandardTextPanel : MonoBehaviour {
+    public Text talkerName;
+    public Text textPanel;
+    public string inputText;
+    public string inputName;
+    public bool start;
+    public bool active;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(start == true)
+        {
+            if(active == false)
+            {
+                start = false;
+                StopCoroutine(StartInuput());
+                StartCoroutine(StartInuput());
+                active = true;
+            }
+        }
+	}
+    public IEnumerator StartInuput()
+    {
+        textPanel.text = "";
+        talkerName.text = inputName;
+        foreach (char letter in inputText.ToCharArray())
+        {
+            textPanel.text += letter;
+            yield return null;
+        }
+        active = false;
+    }
+}
