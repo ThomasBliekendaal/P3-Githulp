c3c97b5921e6c98784decce641c702c9ffc4e6ee
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2018-03-19 14:30:21 +0100

    shit goed

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Thomas/TbScripts/TbWandWep.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TbWandWep : TbWeaponScript {
+
+	// Use this for initialization
+	void Start () {
+        canFire = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Fire();
+	}
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Thomas/TbScripts/TbWeaponScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TbWeaponScript : MonoBehaviour {
+    public float damage;
+    public Transform barrel;
+    public float fireRate;
+    public GameObject projectile;
+    public float aoe;
+    public bool aoeToggle;
+    public float projectileVel;
+    public bool auto;
+    public bool raycast;
+    public bool canFire = true;
+
+	
+    public void Fire()
+    {
+        if (auto == true)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                if (canFire)
+                {
+                    ShootBullet();
+                    canFire = false;
+                    StartCoroutine(Rof());
+                }
+            }
+        }
+    }
+
+    void ShootBullet()
+    {
+        GameObject p = Instantiate(projectile, barrel.position, barrel.rotation);
+        p.GetComponent<Rigidbody>().velocity = transform.forward * projectileVel;
+    }
+
+
+    public IEnumerator Rof()
+    {
+        yield return new WaitForSeconds(fireRate);
+        canFire = true;
+    }
+}
