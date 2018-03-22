4d463d181b0b4589d75db9b7dcf9107223490b42
Dilar <dilar1_4@hotmail.com>
2018-03-22 14:02:15 +0100

    ahhhhhhhhhh
    radfvrvf

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Dilar/DrScripts/DrWeaponTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
+public class DrWeaponTemplate : ScriptableObject
+{
+    public Animator animation;
+
+    public int DMG;
+
+    public bool attacking;
+}
