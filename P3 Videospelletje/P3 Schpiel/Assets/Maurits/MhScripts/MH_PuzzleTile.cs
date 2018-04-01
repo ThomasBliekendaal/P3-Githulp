b85317b08f3e7fa9fe527bcd38b784854add8e79
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-04-02 00:19:39 +0200

    Added the puzzle script
    Now I can do 1 puzzle, very easy coded

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_PuzzleTile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH_PuzzleTile : MonoBehaviour {
+    public bool pressed;
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponentInParent<MH_TileCheck>().tiles.Add(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnCollisionEnter(Collision hit)
+    {
+        if(hit.gameObject.tag == "Player")
+        {
+            if (pressed)
+            {
+                gameObject.GetComponentInParent<MH_TileCheck>().ResetTiles();
+            }
+            else
+            {
+                pressed = true;
+                gameObject.GetComponentInParent<MH_TileCheck>().CheckTiles();
+            }
+        }
+    }
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_TileCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH_TileCheck : MonoBehaviour {
+    public List<GameObject> tiles = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ResetTiles()
+    {
+
+    }
+    public void OpenGate()
+    {
+
+    }
+    public void CheckTiles()
+    {
+        for(int i = 0; i < tiles.Count; i++)
+        {
+            if(tiles[i].GetComponent<MH_PuzzleTile>().pressed != true)
+            {
+                return;
+            }
+            OpenGate();
+        }
+    }
+}
