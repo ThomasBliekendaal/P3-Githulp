563c2034b40a1a63c2eafd5cc398f5fb1186d613
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-04-04 12:25:16 +0200

    CodeLock progress
    Yep

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_CodeLock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MH_CodeLock : MonoBehaviour {
+    public GameObject code;
+    public GameObject codeUI;
+    public int solution;
+    public string convSol;
+	// Use this for initialization
+	void Start () {
+        solution = Random.Range(1000, 99999);
+        convSol = solution.ToString();
+        code.GetComponent<TextMesh>().text = solution.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Interact()
+    {
+        codeUI.SetActive(true);
+    }
+    public void CheckCode()
+    {
+        solution.ToString();
+        if (convSol == codeUI.GetComponentInChildren<Text>().text)
+        {
+            Open();
+        }
+    }
+    public void Open()
+    {
+        print("Hi");
+    }
+    public void CloseUI()
+    {
+
+    }
+}
