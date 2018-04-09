8e9d8481762873ac02c0ce77105cc92828691e09
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-04-09 15:44:31 +0200

    Hotfix GridPuzzle, minors
    Made a stupid mistake with it, Now all puzzles work, they only need the effect if it opens before it can be fully used.

--- P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_TileCheck.cs
+++ P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_TileCheck.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MH_TileCheck : MonoBehaviour {
     public List<GameObject> tiles = new List<GameObject>();
+    public Vector3 resetPos;
 	// Use this for initialization
 	void Start () {
 
@@ -15,7 +16,11 @@ public class MH_TileCheck : MonoBehaviour {
 	}
     public void ResetTiles()
     {
-
+        GameObject.FindGameObjectWithTag("Player").transform.position = resetPos;
+        for(int i = 0; i < tiles.Count; i++)
+        {
+            tiles[i].GetComponent<MH_PuzzleTile>().pressed = false;
+        }
     }
     public void OpenGate()
     {
