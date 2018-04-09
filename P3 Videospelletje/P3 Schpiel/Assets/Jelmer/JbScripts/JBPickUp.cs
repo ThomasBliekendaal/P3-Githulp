d8445e1c0409ef167c50fb2d97a01a408015776f
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-04-09 12:42:23 +0200

    Did something

--- P3 Videospelletje/P3 Schpiel/Assets/Jelmer/JbScripts/JBPickUp.cs
+++ P3 Videospelletje/P3 Schpiel/Assets/Jelmer/JbScripts/JBPickUp.cs
@@ -6,6 +6,7 @@ public class JBPickUp : JBInteractable
 {
     public GameObject pickUp;
     public JBCharacterMovement player;
+    public Vector3 pickupRotation;
     public Transform hand;
 
     public void Start()
@@ -23,14 +24,28 @@ public class JBPickUp : JBInteractable
             activeItem.GetComponent<Rigidbody>().isKinematic = false;
             activeItem.transform.parent = null;
             activeItem.GetComponent<Collider>().isTrigger = false;
+            activeItem.GetComponent<JBThrowItem>().enabled = false;
 
             player.currentItem[0] = gameObject;
-            gameObject.GetComponent<JBPickUp>().enabled = true;
+            gameObject.GetComponent<JBPickUp>().enabled = false;
             gameObject.GetComponent<Rigidbody>().isKinematic = true;
             gameObject.transform.parent = hand;
             gameObject.GetComponent<Collider>().isTrigger = true;
             gameObject.transform.position = hand.position;
             gameObject.transform.rotation = hand.rotation;
+            gameObject.GetComponent<JBThrowItem>().enabled = true;
+            player.currentItem[0] = gameObject;
+        }
+        else
+        {
+            gameObject.GetComponent<JBPickUp>().enabled = false;
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.transform.parent = hand;
+            gameObject.GetComponent<Collider>().isTrigger = true;
+            gameObject.transform.position = hand.position;
+            transform.localRotation = Quaternion.Euler(new Vector3(pickupRotation.x,pickupRotation.y,pickupRotation.z));
+            gameObject.GetComponent<JBThrowItem>().enabled = true;
+            player.currentItem = new GameObject[1] {gameObject};
         }
     }
 }
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Jelmer/JbScripts/JBThrowItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JBThrowItem : MonoBehaviour {
+
+    public float maxForce;
+    public JBCharacterMovement player;
+    public Transform hand;
+    private float power;
+    public float chargespeed;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player").GetComponent<JBCharacterMovement>();
+        hand = GameObject.FindWithTag("GameController").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire2") && power <= maxForce)
+        {
+            
+            power += Time.deltaTime * chargespeed;
+        }
+        else if (Input.GetButtonUp("Fire2"))
+        {
+            ThrowItem(power);
+            power = 0;
+        }
+	}
+
+    public void ThrowItem(float force)
+    {
+        player.currentItem = new GameObject[0];
+        gameObject.GetComponent<Collider>().isTrigger = false;
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.AddForce(hand.forward * power);
+        gameObject.GetComponent<JBThrowItem>().enabled = false;
+        gameObject.GetComponent<JBPickUp>().enabled = true;
+        transform.parent = null;
+    }
+}
