8b4807b4ab8521db59679d7159d66cdfc6597d9d
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-04-09 16:59:05 +0200

    Had to stop working
    Yep

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_HallStand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH_HallStand : MonoBehaviour {
+    public bool canSpawnNewText;
+    public GameObject textBox;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTriggerStay(Collider hit)
+    {
+        if (canSpawnNewText)
+        {
+            Delay(1);
+        }
+    }
+    public IEnumerator Delay(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+    }
+}
