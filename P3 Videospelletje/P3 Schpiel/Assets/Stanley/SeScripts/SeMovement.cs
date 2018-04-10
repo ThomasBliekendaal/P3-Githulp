ae1eb28add82fe485817e2a5a673562c3bcae587
StanleyvanEssen <stanleyessen2001@github.com>
2018-04-10 15:24:42 +0200

    Added some stuff (once again)

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Stanley/SeScripts/SeMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeMovement : MonoBehaviour
+{
+    public Vector3 movement;
+    public float speed;
+    public float h;
+    public float v;
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        movement.x = h;
+        movement.z = v;
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Stanley/SeScripts/SeRotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeRotator : MonoBehaviour
+{
+    public Vector3 v;
+    public float hor;
+    public float ver;
+    public float speed;
+    public Vector3 v2;
+
+    void Update()
+    {
+        Cursor.visible = false;
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+        v.x = hor;
+        v.y = ver;
+        transform.Translate(v * speed * Time.deltaTime);
+        v2.y = Input.GetAxis("Mouse X");
+        transform.Rotate(v2);
+    }
+}
