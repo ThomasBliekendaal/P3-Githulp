fb1db8563920933bf96495fa0db534d1461521d0
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2018-04-10 09:39:02 +0200

    Added the dialogscript
    Accidentally named it HallStand because I was testing it on that...

--- P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_HallStand.cs
+++ P3 Videospelletje/P3 Schpiel/Assets/Maurits/MhScripts/MH_HallStand.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MH_HallStand : MonoBehaviour {
     public bool canSpawnNewText;
-    public GameObject textBox;
+    private bool firstTime = true;
+    public bool enabledd;
+    public string[] dialog;
+    public string nameOf;
+    public GameObject dialogText;
+    public GameObject dialogMenu;
+    public GameObject dialogName;
+    public int current;
+    public float delay;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,17 +21,53 @@ public class MH_HallStand : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-    public void OnTriggerStay(Collider hit)
-    {
-        if (canSpawnNewText)
+        if (Input.GetButton("Interact"))
         {
-            Delay(1);
+            delay = 0;
+        }
+        else
+        {
+            delay = 0.1f;
+        }
+        if (Input.GetButtonDown("Interact"))
+        {
+            if (canSpawnNewText && firstTime == false)
+            {
+                if (current >= dialog.Length)
+                {
+                    dialogMenu.SetActive(false);
+                    enabledd = false;
+                    current = 0;
+                    firstTime = true;
+                }
+                if (enabledd)
+                {
+                    StartCoroutine(Typewriter());
+                    canSpawnNewText = false;
+                }
+            }
+        }
+	}
+    public void OnTriggerEnter(Collider hit)
+    {
+        if (canSpawnNewText && enabledd && firstTime)
+        {
+            firstTime = false;
+            dialogMenu.SetActive(true);
+            dialogName.GetComponent<Text>().text = nameOf;
+            StartCoroutine(Typewriter());
+            canSpawnNewText = false;
         }
     }
-    public IEnumerator Delay(float wait)
+    public IEnumerator Typewriter()
     {
-        yield return new WaitForSeconds(wait);
+        dialogText.GetComponent<Text>().text = null;
+        for(int i = 0; i < dialog[current].Length; i++)
+        {
+            dialogText.GetComponent<Text>().text += dialog[current][i];
+            yield return new WaitForSeconds(delay);
+        }
+        current++;
+        canSpawnNewText = true;
     }
 }
