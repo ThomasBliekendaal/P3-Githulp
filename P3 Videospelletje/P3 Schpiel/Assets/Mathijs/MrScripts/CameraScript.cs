fe7953dfaccd9a05fa3e05c13ed4af4e68f96db9
MatthijsRisch <matthijs_r555@hotmail.com>
2018-04-16 12:47:51 +0200

    kip

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Mathijs/MrScripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public Vector3 camerarondkijken;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camerarondkijken.x += -Input.GetAxis("Mouse Y");
+        camerarondkijken.x = Mathf.Clamp(camerarondkijken.x, -50.0f, 50.0f);
+        transform.eulerAngles = (new Vector3(camerarondkijken.x, transform.eulerAngles.y, 0.0f));
+    }
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Mathijs/MrScripts/MrCharacterScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrCharacterScript : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+    public Vector3 movement;
+    public Vector3 jumpVector;
+    public Vector3 lookAround;
+    public float hor;
+    public float ver;
+    public float speed;
+    public bool run;
+    public float walkingSpeed;
+    public float runningSpeed;
+    public bool mayJump;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+        if (Input.GetButtonDown("Sprint"))
+        {
+            run = true;
+        }
+        if (Input.GetButtonUp("Sprint"))
+        {
+            run = false;
+        }
+        transform.Translate(movement * speed);
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+        movement.x = hor;
+        movement.z = ver;
+        if (run == true)
+        {
+            speed = runningSpeed;
+        }
+        if (run == false)
+        {
+            speed = walkingSpeed;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (mayJump == true)
+            {
+                rigidbody.velocity = jumpVector;
+                mayJump = false;
+            }
+
+        }
+        lookAround.y -= -Input.GetAxis("Mouse X");
+        transform.eulerAngles = (new Vector3(transform.eulerAngles.x, lookAround.y, 0.0f));
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain")
+        {
+            mayJump = true;
+        }
+    }
+}
