17194f8e01ca2693c893d3754965cce027e23684
StanleyvanEssen <stanleyessen2001@github.com>
2018-04-22 02:46:16 +0200

    Audio toegevoegt en kan switchen van boss fight naar  ig overworld

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Stanley/SeScripts/SeAudioChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeAudioChange : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (GameObject.FindWithTag("AudioChange").GetComponent<BoxCollider>().enabled == true)
+        {
+            gameObject.GetComponent<AudioSource>().enabled = true;
+        }
+        if (GameObject.FindWithTag("AudioChange").GetComponent<BoxCollider>().enabled == false)
+        {
+            gameObject.GetComponent<AudioSource>().enabled = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        gameObject.GetComponent<AudioSource>().enabled = true;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        GameObject.FindWithTag("AudioChange").GetComponent<BoxCollider>().enabled = true;
+    }
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Stanley/SeScripts/SeAudioChange2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeAudioChange2 : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (GameObject.FindWithTag("AudioChange2").GetComponent<BoxCollider>().enabled == true)
+        {
+            gameObject.GetComponent<AudioSource>().enabled = true;
+        }
+        if (GameObject.FindWithTag("AudioChange2").GetComponent<BoxCollider>().enabled == false)
+        {
+            gameObject.GetComponent<AudioSource>().enabled = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        gameObject.GetComponent<AudioSource>().enabled = false;
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        GameObject.FindWithTag("AudioChange2").GetComponent<BoxCollider>().enabled = true;
+    }
+}
