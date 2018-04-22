e4a39381a0a50d967536168f2e48b9f333e6b653
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2018-04-22 17:16:58 +0200

    enemybullshit en portal

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Thomas/TbScripts/TbBigDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TbBigDoor : TbInteractables {
+    public GameObject doorLeft;
+    public GameObject doorRight;
+    public bool doorOpen;
+
+	// Use this for initialization
+	void Start () {
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (interacted == true)
+        {
+            if (doorOpen == false)
+            {
+                doorLeft.transform.Rotate(0, -90, 0, Space.World);
+                doorRight.transform.Rotate(0, 90, 0, Space.World);
+                doorLeft.GetComponent<TbBigDoor>().doorOpen = true;
+                doorRight.GetComponent<TbBigDoor>().doorOpen = true;
+                doorLeft.GetComponent<TbBigDoor>().interacted = true;
+                doorRight.GetComponent<TbBigDoor>().interacted = true;
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            doorLeft.transform.Rotate(0, 0, 0, Space.World);
+            doorRight.transform.Rotate(0, 180, 0, Space.World);
+        }
+	}
+}
--- P3 Videospelletje/P3 Schpiel/Assets/Thomas/TbScripts/TbEnemyScript.cs
+++ P3 Videospelletje/P3 Schpiel/Assets/Thomas/TbScripts/TbEnemyScript.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TbEnemyScript : MonoBehaviour {
     public float enemyHealth;
     public float enemyDamage;
+    public NavMeshAgent agent;
+    public Transform target;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        agent = this.GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        agent.SetDestination(target.position);
+
+    }
     public void TakeDamage(float damage)
     {
         enemyHealth -= damage;
