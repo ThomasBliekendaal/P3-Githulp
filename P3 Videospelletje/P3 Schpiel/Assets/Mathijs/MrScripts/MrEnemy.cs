bbaaee5e60698e15a75725533a0d3e46df389cb9
MatthijsRisch <matthijs_r555@hotmail.com>
2018-04-22 15:45:17 +0200

    enemies

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Mathijs/MrScripts/MrEnemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrEnemy : MonoBehaviour {
+    public int moveSpeed = 4;
+    public bool start;
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (start == true)
+        {
+            transform.LookAt(player);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            start = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            start = false;
+        }
+    }
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Mathijs/MrScripts/MrNordicAxeScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MrNordicAxeScript : MonoBehaviour {
+
+	public bool active;
+    public Collider c;
+    public int score;
+    public string scoreText;
+    public GameObject textObject;
+    public bool win;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            if(active == false)
+            {
+                active = true;
+                StartCoroutine(S());
+            }
+        }
+        if (score == 3)
+        {
+            win = true;
+        }
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (active == true)
+        {
+            if (collision.gameObject.tag == ("Enemy"))
+            {
+                score += 1;
+                Destroy(collision.gameObject);
+                scoreText = score.ToString();
+            }
+        }
+    }
+
+    public IEnumerator S()
+    {
+        c.isTrigger = false;
+        transform.Rotate(new Vector3(45, 0, 0));
+        yield return new WaitForSeconds(0.2f);
+        Back();
+    }
+
+    public void Back()
+    {
+        c.isTrigger = false;
+        transform.Rotate(new Vector3(-45, 0, 0));
+        active = false;
+    }
+}
