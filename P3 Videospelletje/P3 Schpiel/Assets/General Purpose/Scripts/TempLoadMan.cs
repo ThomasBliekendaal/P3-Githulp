8b8888999fdb43c5bd0b3de910dc2647c86b8b0e
ThomasBliekendaal <33858982+ThomasBliekendaal@users.noreply.github.com>
2018-04-22 20:55:50 +0200

    shit kan geladen worden vanuit hubworld

---
+++ P3 Videospelletje/P3 Schpiel/Assets/General Purpose/Scripts/TempLoadMan.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TempLoadMan : MonoBehaviour {
+    public GameObject[] buttons;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Medieval()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Dungeon()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Steampunk()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Nordic()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Gothic()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Space()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+}
