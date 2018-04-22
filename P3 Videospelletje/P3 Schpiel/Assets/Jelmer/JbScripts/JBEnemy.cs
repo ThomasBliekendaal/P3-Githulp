3bd6eef1c0b8e9ff4714fee03807f5026e47a3f2
JelmerBatenburg <33858985+JelmerBatenburg@users.noreply.github.com>
2018-04-22 19:51:03 +0200

    portal voor thomas

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Jelmer/JbScripts/JBEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class JBEnemy : MonoBehaviour {
+    public Transform player;
+    public NavMeshAgent agent;
+
+    public void Move()
+    {
+        agent.SetDestination(player.position);
+    }
+
+	public virtual void Attack()
+    {
+
+    }
+
+    public IEnumerator AttackTimer()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ P3 Videospelletje/P3 Schpiel/Assets/Jelmer/JbScripts/JBWeaponSwing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JBWeaponSwing : MonoBehaviour {
+    public bool startSwing;
+    public bool returnSwing;
+    public bool active;
+    public Vector3 rot;
+
+    public void Update()
+    {
+        if (startSwing)
+        {
+            transform.Rotate(new Vector3(0, -170, -100) * Time.deltaTime * 2);
+        }
+        else if(returnSwing)
+        {
+            transform.Rotate(new Vector3(0, 170, 100) * Time.deltaTime * 2);
+        }
+        if (Input.GetButtonDown("Fire1") && !active)
+        {
+            active = true;
+            StartCoroutine(StartSwing());
+        }
+    }
+
+    public IEnumerator StartSwing()
+    {
+        startSwing = true;
+        yield return new WaitForSeconds(0.35f);
+        startSwing = false;
+        StartCoroutine(ReturnSwing());
+    }
+
+    public IEnumerator ReturnSwing()
+    {
+        returnSwing = true;
+        yield return new WaitForSeconds(0.35f);
+        returnSwing = false;
+        active = false;
+        SetRotation();
+    }
+
+    public void SetRotation()
+    {
+        transform.localRotation = Quaternion.Euler(new Vector3(270, 0, 90));
+    }
+}
