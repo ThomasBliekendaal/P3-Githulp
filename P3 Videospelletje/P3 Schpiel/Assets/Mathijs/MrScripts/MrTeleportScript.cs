95d57b7477d9e00a14fbc5cb8c926b1f8d8fc720
MatthijsRisch <matthijs_r555@hotmail.com>
2018-04-23 08:50:22 +0200

    alles doet het!

---
+++ P3 Videospelletje/P3 Schpiel/Assets/Mathijs/MrScripts/MrTeleportScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MrTeleportScript : MonoBehaviour {
+    public MrNordicAxeScript script;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (script.win == true)
+        {
+            meh();
+        }
+	}
+
+    public void meh ()
+    {
+        GameObject.FindGameObjectWithTag("StatSaver").GetComponent<StatManager>().AddPendant(4);
+        SceneManager.LoadScene("HubWorld");
+    }
+}
